76538584957f75e33f61556c967dd9a9fe31522f
XiaokangRan <915009286@qq.com>
2019-10-27 05:43:31 +0800

    'update'

--- src/website/baike/movie.py
+++ src/website/baike/movie.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#coding=utf-8
 #
 import requests
 from bs4 import BeautifulSoup
@@ -16,7 +18,8 @@ class HtmlDownloader(object):
             return None
         return response.content
 
-url = 'https://baike.baidu.com/item/%E8%94%A1%E5%BE%90%E5%9D%A4'
+url = 'https://baike.baidu.com/item/%E8%94%A1%E5%BE%90%E5%9D%A4'   #actor
+# url = 'https://baike.baidu.com/item/%E6%88%98%E7%8B%BC%E2%85%A1?fromtitle=%E6%88%98%E7%8B%BC2&fromid=17196087#3' #movie
 download = HtmlDownloader()
 contents = download.download(url)
 selector=Selector(text=contents)
@@ -34,4 +37,20 @@ for i, name in enumerate(names):
     lines += title + ',' + name + ',' + value + '\n'
     print(title + ',' + name + ',' + value + '\n')
 
-/html/body/div[4]/div[4]/div/div[1]/div[1]/div[25]
+# Selector(text=response.content.decode('utf-8')).xpath('//div[contains(@class, "main-content")]')
+#role
+# role = selector.xpath('//div[contains(@class, "role-name")]').extract()
+
+role = selector.xpath('//div[contains(@class, "lemmaWgt-roleIntroduction")]').extract()
+
+role_name = Selector(text=role[0]).xpath('//span[contains(@class, "item-key")]').extract()
+role_actor = Selector(text=role[0]).xpath('//div[contains(@class, "item-value")]').extract()
+for i, role_text in enumerate(role_name):
+    # role
+    role_text = role_name[0]
+    Selector(text=role_text)
+    temp = Selector(text=role).xpath('//dt/text()|//dt/a/text()').extract()
+
+<div class="role-name">
+< div class ="role-actor" >
+<dd class="role-description">
